7b5bfc2c2ca7c4437dd2eb64f15f2f9da836930a
Harshavardhan <hvardhan1024@gmail.com>
2024-12-13 13:10:13 +0530

    garden scene added

--- src/components/SceneLoader.js
+++ src/components/SceneLoader.js
@@ -7,4 +7,15 @@ let currentScene = null
 export function loadScene(){
 
     createGardenScene()
+
+    createNavigationButtons()
+}
+
+
+function createNavigationButtons(){
+    const navContainer = document.getElementById('navigation') || document.createElement('div');
+    navContainer.id = 'navigation'
+    navContainer.innerHTML = `<button onclick="loadScene('garden')">Garden</button>`
+    
+    document.body.appendChild(navContainer);
 }
