e744491cddd8a3101640421b18a1e6106a817174
Harshavardhan <hvardhan1024@gmail.com>
2024-12-13 12:42:40 +0530

    added logo ico and png and double click func

--- src/main.js
+++ src/main.js
@@ -1,24 +1,32 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+// import { loadScene } from './components/SceneLoader';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+window.addEventListener('resize',()=>{
+    sizes.width = window.innerWidth
+    sizes.height=window.innerHeight
 
-setupCounter(document.querySelector('#counter'))
+    camera.aspect= sizes.width/sizes.height
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(sizes.width, sizes.height)
+
+})
+// full screen
+window.addEventListener('dblclick',()=>{
+    console.log('hello')
+    if(!document.fullscreenElement){
+        document.body.requestFullscreen()
+    }else{
+        document.exitFullscreen()
+    }
+})
+
+
+// loadScene('garden'); // Load the initial scene
+// window.loadScene = loadScene; // Expose function globally for button click handling
